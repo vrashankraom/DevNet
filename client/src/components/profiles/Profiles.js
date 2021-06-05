5582cb1f5093670165e062776d940b208c65e251
Vrashank Rao M <vrashankrao@gmail.com>
2021-06-05 12:35:14 +0530

    Trying Deployment-2

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -25,7 +25,7 @@ useEffect(()=>{
                 {loading? (<Spinner/>) : (<Fragment>
                     <h1 className="large text-primary">Developers</h1>
                     <p className="lead">
-                        <i className="fab fa-connectdevelop"></i>Browse and connect with Developers
+                        <i className="fab fa-connectdevelop"></i> Browse and connect with Developers
                     </p>
                     <div className="form form-group">
                     <div className="form-group">
